dd763f54c697093023cf88a496172d5ebfbf63f8
Matthew Harrison-Jones <matthojo@gmail.com>
2015-08-07 15:15:59 +0100

    Allow custom presets to be used

--- README.md
+++ README.md
@@ -6,10 +6,13 @@ Basic usage:
 `qa-report`
 
 Output markdown to file:
+
 `qa-report -o <path to file>`
 
 Choose a preset to run (this can be a path to a preset `.json` file):
+
 `qa-report -p <preset name|path>`
 
 This will output the markdown into a GitHub comment on the specified issue/PR ID:
+
 `qa-report -i <GitHub issue ID>`
--- src/preset.js
+++ src/preset.js
@@ -10,7 +10,27 @@ function preset (name) {
 }
 
 function init () {
-  var options = JSON.parse(fs.readFileSync(__dirname + '/presets/' + presetName + '.json', 'utf8'))
+  var options = {}
+  try {
+    if (fs.lstatSync(presetName).isFile()) {
+      if (presetName.substr((~-presetName.lastIndexOf(".") >>> 0) + 2) === 'json') {
+        options = JSON.parse(fs.readFileSync(presetName, 'utf8'))
+      } else {
+        console.log('Preset path must be a .json file.')
+        return process.exit(1)
+      }
+    }
+  }
+  catch (e) {
+    try {
+      options = JSON.parse(fs.readFileSync(__dirname + '/presets/' + presetName + '.json', 'utf8'))
+    }
+    catch (e) {
+      console.log('This preset does not exist.')
+      return process.exit(1)
+    }
+  }
+
   presetOptions = options
 }
 module.exports = preset
