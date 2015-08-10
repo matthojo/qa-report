effec705b775ef56ade676d10973b6ee82a713b0
Ben Parnell <benjaminparnell.94@gmail.com>
2015-08-10 21:57:07 +0100

    Get -r automatically if in .git folder. Closes #9

--- src/post-to-pr.js
+++ src/post-to-pr.js
@@ -1,27 +1,25 @@
 var ghIssues = require('ghissues')
   , osHomedir = require('os-homedir')
   , fs = require('fs')
+  , getRepoAndOwner = require('./get-repo-and-owner')
   , configPath = osHomedir() + '/.qa-report.json'
 
 function postToPR(id, repo, body, callback) {
   var config
   
   try {
-    config = JSON.parse(fs.readFileSync(osHomedir() + '/.qa-report.json', 'utf8'))
+    config = JSON.parse(fs.readFileSync(configPath))
   } catch (e) {
     return callback(new Error('Can\'t read ' + configPath + '. Ensure it is present and is valid JSON.'))
   }
 
-  repo = repo.split('/')
-
-  if (repo.length !== 2) {
-    return callback(new Error('repo must be provided as <owner>/<repoName>'))
-  }
-
-  var repoName = repo[1]
-    , owner = repo[0]
-
-  ghIssues.createComment(config, owner, repoName, id, body, callback)
+  getRepoAndOwner(repo, function (error, repoName, owner) {
+    if (error) return callback(error)
+    ghIssues.createComment(config, owner, repoName, id, body, function (error) {
+      if (error) return callback(error)                          
+      callback(null, owner + '/' + repoName)
+    })
+  })
 }
 
 module.exports = postToPR
